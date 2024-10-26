4faf7f239e82daf3098064223812e9d5b4dc9b82
Linh Nguyen (Hailey) <112211984+lhn004@users.noreply.github.com>
2024-10-26 13:22:40 -0400

    Create ex4-solutions.py

---
+++ ex4-solutions.py
@@ -0,0 +1,8 @@
+# Ask the user for input. Optionally, you can provide a specific prompt message.
+user_input = input("Please enter your text: ")
+
+# Convert the input text to lowercase using the .lower() method.
+lowercase_output = user_input.lower()
+
+# Print the lowercase version of the input.
+print(lowercase_output)
